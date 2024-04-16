cbdb9b034cfe1580a6b9c6ee66b4497224b3fd4a
Usama <osamaimran25@gmail.com>
2024-04-16 16:50:47 +0500

    python version upgraded

--- biomhandler.py
+++ biomhandler.py
@@ -1,4 +1,9 @@
-import os, sys, argparse, numpy, h5py, biom
+import os
+import sys
+import argparse
+import numpy
+import h5py
+import biom
 
 def tojson(input_data):
     table = biom.parse.parse_biom_table(input_data)
@@ -11,14 +16,11 @@ parser.add_argument('input', metavar='I', type=str, help='input')
 args = parser.parse_args()
 
 if h5py.is_hdf5(args.input):
-  with h5py.File(args.input) as h5_file:
-    json = tojson(h5_file)
-    h5_file.close()
+    with h5py.File(args.input, 'r') as h5_file:
+        json_data = tojson(h5_file)
 else:
-  with open(args.input, 'r') as file:
-    json = file.read()
-    file.close()
+    with open(args.input, 'r') as file:
+        json_data = file.read()
 
-print json
-
+print(json_data)
 sys.exit()
